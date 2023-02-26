41538724249cd20f9daf4f232b6ea2234a3232cd
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-02-26 21:28:59 +0900

    2 way bindind added

--- app/src/main/java/com/example/save_files_sample_app/FileSaver.kt
+++ app/src/main/java/com/example/save_files_sample_app/FileSaver.kt
@@ -7,31 +7,23 @@ import android.widget.Toast
 import java.io.File
 
 
-class FileSaver(val context: Context) {
-    private val fileName: String = "my_file"
-    var fileContent: String = ""
+class FileSaver( context: Context) {
+   
+    private val mContext =context
 
-    fun writeToFile(content: String) {
-        fileContent = content
-        try{
-            context.openFileOutput(fileName, Context.MODE_PRIVATE).use {
-                it.write(fileContent.toByteArray())
-            }
-            Toast.makeText(context, "Saved Successfully!", Toast.LENGTH_SHORT).show()
-        }catch (exception: Exception){
-            Toast.makeText(context, "Error occured while saving!", Toast.LENGTH_SHORT).show()
+    fun writeToFile(fileName:String,dataToSave:String) {
+        mContext.openFileOutput(fileName,Context.MODE_PRIVATE).use {
+            it.write(dataToSave.toByteArray())
         }
 
     }
 
 
 
-    fun readFromFile(): String {
-
-        val readStr = File(context.filesDir, fileName).readText()
-        return readStr
+    fun readFromFile(fileName: String): String  = File(mContext.filesDir, fileName).readText()
 
 
-    }
+
+
 
 }
--- app/src/main/java/com/example/save_files_sample_app/MainActivity.kt
+++ app/src/main/java/com/example/save_files_sample_app/MainActivity.kt
@@ -8,29 +8,34 @@ import com.example.save_files_sample_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     val viewModel: MyViewModel by viewModels()
+    val fileName: String = "tt"
+
+    // private lateinit var viewModel: MyViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
 
         binding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 
-
-        //Data binding
+        // binding
         binding.myVM = viewModel
         binding.lifecycleOwner = this
         binding.buttonsave.setOnClickListener {
-           // viewModel.performsaveText (FileSaver(this).writeToFile(viewModel.savedText.value.toString())
-           // viewModel.performsaveText(binding.editTextTextPersonName.text.toString())
-            FileSaver(this).writeToFile(viewModel.savedText.value.toString())
+
+            //viewModel.upadateFrommsaveText(binding.editTextTextPersonName.text.toString())
+
+            FileSaver(this).writeToFile(fileName, viewModel.typedtext)
+
+
         }
 
 
         binding.button2view.setOnClickListener {
-
-          viewModel.performsaveText(FileSaver(this).readFromFile())
+            val dataRetrived = FileSaver(this).readFromFile(fileName)
+            viewModel.upadateFrommsaveText(dataRetrived)
 
         }
     }
